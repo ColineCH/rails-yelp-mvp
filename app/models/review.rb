7f84ad2513799a9beaf97389204bc606310e3c6a
coline <colnatcoline@hotmail.com>
2016-08-10 16:54:50 +0200

    exercice done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, inclusion: { in: (0..5).to_a }
+  validates :rating, inclusion: { in: (0..5).to_a, :message => "choose a rating between 0 and 5"}
   validates :rating, :numericality => {:only_integer => true}
 end
